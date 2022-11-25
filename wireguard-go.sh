ab33b7d49af3642e5c9dcb809799c8bac79171a6
Lison-Li <56676547+Lison-Li@users.noreply.github.com>
2022-11-25 23:16:10 +0800

    Update wireguard-go.sh

--- wireguard-go.sh
+++ wireguard-go.sh
@@ -71,11 +71,11 @@ esac
 echo -e "${INFO} Architecture: ${OS_ARCH} ${PKGT}"
 
 echo -e "${INFO} Get ${PROJECT_NAME} download URL ..."
-DOWNLOAD_URL=$(curl -fsSL ${GH_API_URL} | grep 'browser_download_url.*gz"' | cut -d'"' -f4 | grep "${FILE_KEYWORD}")
+DOWNLOAD_URL=$(curl --insecure -fsSL ${GH_API_URL} | grep 'browser_download_url.*gz"' | cut -d'"' -f4 | grep "${FILE_KEYWORD}")
 echo -e "${INFO} Download URL: ${DOWNLOAD_URL}"
 
 echo -e "${INFO} Installing ${PROJECT_NAME} ..."
-curl -LS "${DOWNLOAD_URL}" | tar xzC ${BIN_DIR} ${BIN_NAME}
+curl --insecure -LS "${DOWNLOAD_URL}" | tar xzC ${BIN_DIR} ${BIN_NAME}
 chmod +x ${BIN_FILE}
 if [[ ! $(echo ${PATH} | grep ${BIN_DIR}) ]]; then
     ln -sf ${BIN_FILE} /usr/bin/${BIN_NAME}
